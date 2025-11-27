0762bc7ae85a4241873ec8ef24eeca7f48e74bac
Nico Plyley <nico@phoenixtechlab.com>
2025-11-27 13:28:59 -0500

    feat: add hono types for context variables and environment bindings

---
+++ src/lib/server/types/hono.d.ts
@@ -0,0 +1,16 @@
+import { type schema } from '$lib/server/db/schema';
+import type { DrizzleD1Database } from 'drizzle-orm/d1';
+import type { Org } from '$lib/server/services';
+
+declare module 'hono' {
+	interface ContextVariableMap {
+		Org: Org;
+		DB: DrizzleD1Database<typeof schema>;
+	}
+}
+
+export type Env = {
+	Bindings: {
+		DB: D1Database;
+	};
+};
