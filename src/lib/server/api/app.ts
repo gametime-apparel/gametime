d89c83c01b5e2cbab01aeb4c6a153ec3df21f369
Nico Plyley <nico@phoenixtechlab.com>
2025-11-27 15:27:46 -0500

    feat: add global error handler for improved API error responses

--- src/lib/server/api/app.ts
+++ src/lib/server/api/app.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
 import { orgs } from './routes';
 import { contextMiddleware } from '$lib/server/api/middleware';
+import { HTTPException } from 'hono/http-exception';
 
 const app = new Hono().basePath('/api');
 
@@ -14,6 +15,25 @@ app.use('*', contextMiddleware);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app.route('/orgs', orgs);
+
+app.onError((error, c) => {
+	if (error instanceof HTTPException) {
+		console.error(error.cause);
+		// Get the custom response
+		return c.json(
+			{
+				success: false,
+				message: error.message
+			},
+			error.status
+		);
+	}
+
+	return c.json({
+		success: false,
+		error: 'Internal server error'
+	});
+});
 
 export default app;
 export type AppType = typeof routes;
