09eedcd793ae189bebe80e14cd41af998e0488f2
Nico Plyley <nico@phoenixtechlab.com>
2025-11-29 21:12:20 -0700

    test(setup): initialize `Org` service and configure Drizzle ORM in test setup

--- src/lib/test/setup.ts
+++ src/lib/test/setup.ts
@@ -1,8 +1,13 @@
 import { testClient } from 'hono/testing';
 import app, { type AppType } from '$lib/server/api/app.ts';
 import { applyD1Migrations, env } from 'cloudflare:test';
+import { drizzle } from 'drizzle-orm/d1';
+import { schema } from '$lib/server/db/schema.ts';
+import { Org as OrgService } from '$lib/server/services';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 await applyD1Migrations(env.DB, env.TEST_MIGRATIONS);
 export const client = testClient<AppType>(app, env).api;
+
+const db = drizzle(env.DB, { schema });
+
+export const Org = new OrgService(db);
