5bac25c5c68d63bbcecd639c7035cab47b3676d5
Nico Plyley <nico@phoenixtechlab.com>
2025-11-29 21:12:42 -0700

    test(orgs): add comprehensive tests for `GET /orgs/:org` and improve existing tests

--- src/lib/server/api/routes/orgs/Orgs.route.test.ts
+++ src/lib/server/api/routes/orgs/Orgs.route.test.ts
@@ -1,16 +1,23 @@
-import { describe, expect, it } from 'vitest';
-import { client } from '$lib/test/setup';
+import { beforeAll, describe, expect, it } from 'vitest';
+import { client, Org } from '$lib/test/setup';
 import { generateTestTokens } from '$lib/test/defaults.ts';
 
 const token = await generateTestTokens();
 
-describe('Orgs', () => {
-	describe('GET /orgs', () => {
+describe('Orgs', async () => {
+	beforeAll(async () => {
+		await Org.create({
+			name: 'Main Store',
+			slug: 'main'
+		});
+	});
+	describe('GET /orgs', async () => {
 		it('should fail with no token', async () => {
 			const req = await client.orgs.$get();
 
 			const res = await req.json();
 
+			expect(req.status).toBe(401);
 			expect(res.success).toBe(false);
 			expect(res.data).toBe(undefined);
 		});
@@ -27,14 +34,13 @@ describe('Orgs', () => {
 
 			const res = await req.json();
 
-			console.log(res);
-
+			expect(req.status).toBe(200);
 			expect(res.success).toBe(true);
 			expect(res.data).toBeDefined();
 		});
 	});
 
-	describe('POST /orgs', () => {
+	describe('POST /orgs', async () => {
 		it('should fail with no token', async () => {
 			const req = await client.orgs.$post({
 				json: {
@@ -45,6 +51,7 @@ describe('Orgs', () => {
 
 			const res = await req.json();
 
+			expect(req.status).toBe(401);
 			expect(res.success).toBe(false);
 			expect(res.data).toBe(undefined);
 		});
@@ -66,8 +73,34 @@ describe('Orgs', () => {
 
 			const res = await req.json();
 
+			expect(req.status).toBe(201);
 			expect(res.success).toBe(true);
 			expect(res.data).toBeDefined();
+		});
+	});
+	describe('GET /orgs/:org', () => {
+		it('should fail with invalid org', async () => {
+			const req = await client.orgs[':slug'].$get({
+				param: {
+					slug: 'test123'
+				}
+			});
+			const res = await req.json();
+
+			expect(res.success).toBe(false);
+		});
+
+		it('should pass with no token', async () => {
+			const req = await client.orgs[':slug'].$get({
+				param: {
+					slug: 'main'
+				}
+			});
+
+			const res = await req.json();
+
+			expect(req.status).toBe(200);
+			expect(res.success).toBe(true);
 		});
 	});
 });
