32052a410889e164231d9aa14631905d6737bcb5
Nico Plyley <nico@phoenixtechlab.com>
2025-12-14 16:17:28 -0700

    feat(admin): integrate flash message handling

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,8 +1,40 @@
 <script lang="ts">
-	import { Sidebar } from '$lib/components/admin/blocks';
+	import { orgState } from '$lib/stores';
+	import { getFlash } from 'sveltekit-flash-message';
+	import { page } from '$app/state';
+	import toast, { Toaster } from 'svelte-french-toast';
+	
+	const flash = getFlash(page);
+	
+	$effect(() => {
+		if ($flash) {
+			switch ($flash.type) {
+				case 'success':
+					toast.success($flash.message, {
+						style: 'background: #059669; color: #FFF;'
+					});
+					break;
+				case 'error':
+					toast.error($flash.message);
+					break;
+			}
+		}
+	})
+	
 
-	let { children } = $props();
+	let { children, data } = $props();
+	
+	$effect.pre(() => {
+		if (data.orgs) {
+			orgState.set(data.orgs);
+		}
+	})
 </script>
+<div class="min-h-screen w-full transition-colors duration-300 bg-gray-50 text-gray-900 dark:bg-gray-950 dark:text-gray-100">
+<Toaster  />
 
-<Sidebar />
-{@render children()}
+<main>
+	{@render children()}
+</main>
+
+</div>
