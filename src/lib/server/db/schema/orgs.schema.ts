dfa83710e389d4b887ef311563ceb57de4842299
Nico Plyley <nico@phoenixtechlab.com>
2025-12-14 17:53:17 -0700

    feat(server): add validation for `insertOrgSchema` and introduce `selectOrgSchema`

--- src/lib/server/db/schema/orgs.schema.ts
+++ src/lib/server/db/schema/orgs.schema.ts
@@ -1,7 +1,7 @@
 import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
-import { createInsertSchema } from 'drizzle-zod';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
-import { timestamps, archivedAt } from '$lib/server/db/schema/shared.ts';
+import { timestamps, archivedAt } from './shared.ts';
 
 export const orgs = sqliteTable('orgs', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
@@ -13,14 +13,31 @@ export const orgs = sqliteTable('orgs', {
 
 export const schema = { orgs };
 
-export const insertOrgSchema = createInsertSchema(orgs).omit({
+const slugRegex = /^[a-z0-9]+(?:-[a-z0-9]+)*$/;
+
+export const insertOrgSchema = createInsertSchema(orgs, {
+	name: (schema) => schema.min(1, 'Name is required'),
+	slug: (schema) =>
+		schema
+			.min(3, 'Slug must be at least 3 characters')
+			.regex(slugRegex, {
+				message:
+					'Slug must be lowercase, alphanumeric, and cannot contain spaces, double dashes, or start/end with a dash.'
+			})
+}).omit({
 	id: true,
 	createdAt: true,
 	updatedAt: true,
 	archivedAt: true
 });
 
-export const updateOrgSchema = insertOrgSchema.omit({ slug: true }).partial();
+export const updateOrgSchema = insertOrgSchema.partial();
+
+export const selectOrgSchema = createSelectSchema(orgs).omit({
+	updatedAt: true,
+	archivedAt: true
+});
 
 export type NewOrg = z.infer<typeof insertOrgSchema>;
 export type UpdateOrg = z.infer<typeof updateOrgSchema>;
+export type SelectOrg = z.infer<typeof selectOrgSchema>;
