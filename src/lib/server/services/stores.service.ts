9523517ff9689226c9ca8db28ddf743d244cab98
Nico Plyley <nico@phoenixtechlab.com>
2026-02-19 17:33:30 -0500

    feat(admin): added get by slug

--- src/lib/server/services/stores.service.ts
+++ src/lib/server/services/stores.service.ts
@@ -2,11 +2,12 @@ import type { DrizzleD1Database } from 'drizzle-orm/d1';
 import { type schema, stores } from '$lib/server/db/schema';
 import {
 	type CreateStore,
+	type UpdateStore,
 	selectStoreSchema,
 	selectStoresSchema
 } from '$lib/server/contracts/stores.contract';
 import { error } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 
 class StoresService {
 	private readonly db: DrizzleD1Database<typeof schema>;
@@ -45,6 +46,33 @@ class StoresService {
 		}
 
 		return selectStoresSchema.parse(result);
+	}
+
+	public async getBySlug(slug: string) {
+		const result = await this.db.query.stores.findFirst({
+			where: and(eq(stores.orgId, this.orgId), eq(stores.slug, slug))
+		});
+
+		if (!result) {
+			throw error(404, 'Store not found');
+		}
+
+		return selectStoreSchema.parse(result);
+	}
+
+	public async update(slug: string, data: UpdateStore) {
+		const result = await this.db
+			.update(stores)
+			.set(data)
+			.where(and(eq(stores.orgId, this.orgId), eq(stores.slug, slug)))
+			.returning()
+			.get();
+
+		if (!result) {
+			throw error(404, 'Store not found');
+		}
+
+		return selectStoreSchema.parse(result);
 	}
 }
 
