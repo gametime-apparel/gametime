df3c7eba95ba19239922e35aa8e1fb4e258df8ba
Nico Plyley <nico@phoenixtechlab.com>
2026-02-19 17:34:50 -0500

    feat(admin): added current store to layout

--- src/routes/admin/orgs/[orgSlug]/(storeTab)/+layout.server.ts
+++ src/routes/admin/orgs/[orgSlug]/(storeTab)/+layout.server.ts
@@ -1,9 +1,16 @@
 import type { LayoutServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const currentOrg = locals.currentOrg;
+	const currentStore = locals.currentStore;
+
+	if (!currentStore) {
+		throw error(404, 'Store not found');
+	}
 
 	return {
-		currentOrg
+		currentOrg,
+		currentStore
 	};
 };
